16662ce4ce98be59d50092341eff3204a6a61b70
kasperUcl <kbch27791@hotmail.com>
2021-05-24 15:09:34 +0200

    start of template header

---
+++ template/header.php
@@ -0,0 +1,8 @@
+<div class="header">
+  <a href="#default" class="logo">Aros&søn</a>
+  <div class="header-right">
+    <a class="active" href="#communication">Beskeder</a>
+    <a href="#contact">Kontakter</a>
+    <a href="#deals">Aftaler</a>
+  </div>
+</div>
