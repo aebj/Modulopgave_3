e5538ce19ee8a58a363598323798153772dae140
Seje27878 <Seje27878@edu.ucl.dk>
2021-05-25 14:24:10 +0200

    Update communiacation

--- communication.php
+++ communication.php
@@ -4,13 +4,32 @@ include('functions.php');
 
 ?>
 
+<link rel="stylesheet" href="css/communication.css">
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>communication</title>
   </head>
   <body>
 
+      <div class="centralised">
+      <div class="chathistory"></div>
+      <div class="chatbox">
+
+          <form action="" method="post">
+
+              <textarea class="txtarea" id="message" name="message"></textarea>
+
+        </forum>
+
+    </div>
+    </div>
+
+
   </body>
 </html>
+
+<input type="text" name="note" value="">
+<button type="submit">Send</button>
